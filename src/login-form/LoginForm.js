3ef2b2b8d61e90b0d3e9bb7f9ebb1a15017b4907
mathieu_griffoul <mgriffoul@oui.sncf>
2019-10-20 22:04:42 +0200

    Authent working, need clean code

--- src/login-form/LoginForm.js
+++ src/login-form/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from "@material-ui/core/TextField";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {Fab} from "@material-ui/core";
@@ -29,27 +29,37 @@ const useStyles = makeStyles(theme => ({
 
 }));
 
-
-
 function LoginForm() {
 
+    const [loggingState, setLoggingState] = useState("");
+
+    const [username, setLoggin] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogginChange = (e) => {
+        console.log(e.target.value);
+        setLoggin(e.target.value)
+    }
+    const handlePasswordChange = (e) => {
+        console.log(e.target.value);
+        setPassword(e.target.value)
+    }
     const classes = useStyles();
 
-    const handleClick = function () {
-
+    const handleSubmit = function (e) {
+        e.preventDefault();
+        setLoggingState("logging");
         let token = "";
         axios.post('https://localhost:8443/authenticate',
             {
-                username:"toto",
-                password:"tata",
+                username,
+                password,
                 timeout: 1000,
                 headers: {
                     'Content-Type': 'application/json'
                 },
             })
             .then(function (response) {
-
-                console.log(response.data.token);
                 token = response.data.token;
                 // handle success
                 axios.get('https://localhost:8443/test',
@@ -60,20 +70,16 @@ function LoginForm() {
                             'Content-Type': 'application/json'
                         },
                     })
-                    .then(function (response) {
+                    .then(function (response2) {
                         // handle success
-                        console.log('GET MESSAGE : '+ response.data);
+                        console.log('GET MESSAGE : ' + response2.data);
                     });
             });
-
-
-
     };
-
 
     return (
         <div id={"main"} className={classes.root}>
-            <form className={classes.container}>
+            <form className={classes.container} onSubmit={handleSubmit}>
                 <Logo/>
                 <TextField
                     id="login"
@@ -82,6 +88,7 @@ function LoginForm() {
                     type="text"
                     margin="normal"
                     variant="outlined"
+                    onChange={handleLogginChange}
                 />
                 <TextField
                     id="password"
@@ -90,11 +97,14 @@ function LoginForm() {
                     type="password"
                     margin="normal"
                     variant="outlined"
+                    onChange={handlePasswordChange}
                 />
-                <Fab variant="extended" size="small" color="primary" aria-label="delete" className={classes.button}
-                     onClick={handleClick}>
+                <Fab variant="extended" size="small" type={'submit'} color="primary" aria-label="delete"
+                     className={classes.button}>
                     Connect
                 </Fab>
+
+                {loggingState}
             </form>
         </div>
     );
